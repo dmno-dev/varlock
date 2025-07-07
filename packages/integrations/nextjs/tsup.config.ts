1a03a030a39b518c4f4409386718a094b80daa4f
Theo Ephraim <theo@dmno.dev>
2025-07-07 12:15:05 -0700

    nextjs integration v1
    First version of a nextjs integration.
    
    This uses a combination of a drop-in replacement for @next/env, and a config plugin (which injects a webpack plugin) to provde
    
    - loading of .env files via varlock with validation
    - automatic type generation (both for process.env and varlock/env imports
    - log redaction
    - leak prevention during the build
    - leak prevention at runtime

---
+++ packages/integrations/nextjs/tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: [ // Entry point(s)
+    'src/next-env-compat.ts',
+    'src/plugin.ts',
+
+    'src/patch-next-runtime.ts',
+  ],
+
+  dts: true,
+
+  // minify: true, // Minify output
+  sourcemap: true, // Generate sourcemaps
+  treeshake: true, // Remove unused code
+
+  clean: true, // Clean output directory before building
+  outDir: 'dist', // Output directory
+
+  format: ['cjs'], // Output format(s)
+  splitting: false,
+});
