0ef22e92f9641ff0144f40aeeff5a4f151d16802
Theo Ephraim <theo@dmno.dev>
2025-07-28 10:46:49 -0700

    vite + vue example

--- example-monorepo/packages/vanilla-vite-example/src/main.ts
+++ example-monorepo/packages/vanilla-vite-example/src/main.ts
@@ -5,8 +5,7 @@ import { ENV } from 'varlock/env';
 // const ENV = {};
 
 console.log('ITEM1 = ', ENV.ITEM1);
-// console.log('ITEM1 = ', ENV.SECRET_FOO);
-
+// console.log('SECRET_FOO = ', ENV.SECRET_FOO);
 console.log(import.meta.env.APP_ENV);
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
@@ -27,12 +26,11 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <tr><td>SECRET_FOO</td><td>${import.meta.env.SECRET_FOO}</td><td>❌ triggers error</td></tr>
       <tr><td>BAD_KEY</td><td>${import.meta.env.BAD_KEY}</td><td>❌ triggers error</td></tr>
     </table>
-    <div>
-      <p>
-        Should not be transformed:<br/>
-        ENV.PUBLIC_FOO, import.meta.env.PUBLIC_FOO, process.env.PUBLIC_FOO
-      </p>
-    </div>
+    <p>
+      These should not be replaced:<br/>
+      - import.meta.env.VITE_ENV_SPECIFIC_ITEM<br/>
+      - ENV.VITE_ENV_SPECIFIC_ITEM
+    </p>
 
     <div class="card">
       <button id="counter" type="button"></button>
