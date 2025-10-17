7149e92b27035f5b24dbc38f88a30e207d59785d
Theo Ephraim <theo@dmno.dev>
2025-10-17 15:05:50 -0700

    fixes toggle comment behaviour (#170)

--- packages/vscode-plugin/src/extension.ts
+++ packages/vscode-plugin/src/extension.ts
@@ -3,12 +3,14 @@ import { ExtensionContext } from 'vscode';
 
 import { addHoverProvider } from './hover-provider';
 import { addLanguageConfig } from './lang-config';
+import { addToggleCommentsCommand } from './toggle-comments';
 
 // ---
 export function activate(context: ExtensionContext) {
   console.log('Activated @env-spec language plugin');
   addLanguageConfig(context);
   addHoverProvider(context);
+  addToggleCommentsCommand(context);
 }
 
 // This method is called when your extension is deactivated
