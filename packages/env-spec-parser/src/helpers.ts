82a7340a695d62a40c908c37432c6d9cfd7e2c3d
Phil Miller <phil@dmno.dev>
2025-12-01 15:32:49 -0500

    don't coerce out of bounds numbers (#219)
    * don't coerce out of bounds numbers
    
    * changeset
    
    * more number handling tweaks
    
    - fix handling of negative numbers
    - infer string if the number would change at all
    
    ---------
    
    Co-authored-by: Theo Ephraim <theozero@gmail.com>

--- packages/env-spec-parser/src/helpers.ts
+++ packages/env-spec-parser/src/helpers.ts
@@ -1,11 +1,30 @@
-const VALID_NUMBER_REGEX = /^(0|([1-9][0-9]*))?(\.[0-9]+)?$/;
-export function autoCoerce(valStr) {
+// this regex helps avoid any weird numbers
+// - leading zeros
+// - scientific notation
+// - multiple decimal points
+const VALID_NUMBER_REGEX = /^-?(0|([1-9][0-9]*))?(\.[0-9]+)?$/;
+
+export function autoCoerce(valStr: string): string | number | boolean | undefined {
   if (valStr === 'true') return true;
   if (valStr === 'false') return false;
   if (valStr === 'undefined') return undefined;
   // not handling `null` because its a JS specific thing... we'll try to avoid it altogether
 
-  // special check to avoid weird number coercion like `01`, `1e10`
-  if (VALID_NUMBER_REGEX.test(valStr)) return Number(valStr);
+  // the regex check filters out a lot of ambiguous/weird cases
+  if (VALID_NUMBER_REGEX.test(valStr)) {
+    const num = Number(valStr);
+
+    // Check if we can convert back to a string without any changes
+    // this could be loss in precision or just formatting (extra zeros)
+    const backToString = String(num);
+    if (backToString !== valStr) return valStr;
+
+    // if numbers are too large, we'll leave them as strings
+    if (Number.isInteger(num) && !Number.isSafeInteger(num)) return valStr;
+    if (num > Number.MAX_SAFE_INTEGER || num < Number.MIN_SAFE_INTEGER) return valStr;
+
+    // otherwise we can return the number
+    return num;
+  }
   return valStr;
 }
